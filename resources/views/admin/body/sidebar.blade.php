60e38a3861993d6deefc065d27316bf30ccb34c1
abirmehedi77 <86663285+abirmehedi77@users.noreply.github.com>
2023-11-16 09:20:58 -0400

    Update sidebar.blade.php

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -9,7 +9,7 @@
                     class="avatar-md rounded-circle">
             </div> --}}
             <div class="mt-3">
-                <h4 class="font-size-16 mb-1 text-white">{{ Auth::user()->name }}</h4>
+                {{-- <h4 class="font-size-16 mb-1 text-white">{{ Auth::user()->name }}</h4> --}}
                 <span class="text-muted"><i class="ri-record-circle-line align-middle font-size-14 text-success"></i>
                     <span class="text-color">{{ Auth::user()->name }}</span>
                 </span>
