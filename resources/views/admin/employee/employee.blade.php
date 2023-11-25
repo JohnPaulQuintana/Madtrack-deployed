d37fa56b0375266c4670739efb503f19e1f0301f
abirmehedi77 <86663285+abirmehedi77@users.noreply.github.com>
2023-11-25 04:08:26 -0400

    Update employee.blade.php

--- resources/views/admin/employee/employee.blade.php
+++ resources/views/admin/employee/employee.blade.php
@@ -3,7 +3,7 @@
 {{-- header links --}}
 @section('header-links')
     <meta charset="utf-8" />
-    <title>Dashboard | BIS - Admin & Dashboard</title>
+    <title>Dashboard</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="Themesdesign" name="author" />
