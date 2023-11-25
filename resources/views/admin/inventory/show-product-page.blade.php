1c1fe36267c66a869e1ca49326f1418a30215743
abirmehedi77 <86663285+abirmehedi77@users.noreply.github.com>
2023-11-25 01:39:54 -0400

    Update show-product-page.blade.php

--- resources/views/admin/inventory/show-product-page.blade.php
+++ resources/views/admin/inventory/show-product-page.blade.php
@@ -136,22 +136,20 @@
                                     <form action="{{ route('bulk.manage.stocks') }}" method="POST" style="max-height: 400px; overflow-y: auto;border: 1px solid #ddd;">
                                         @csrf
                                         <table class="table">
-                                            <!-- Fixed header -->
-                                            <div style="background-color: #f5f5f5; padding: 10px; position: sticky; top: 0; z-index: 1;">
-                                                <thead>
-                                                    <tr>
-                                                        <th>Product Type</th>
-                                                        <th>Product Name</th>
-                                                        <th>Product Brand</th>
-                                                        <th>Stocks</th>
-                                                        <th>Price - (pcs)</th>
-                                                        <th>Price - (pck)</th>
-                                                        <th>Product Pack</th>
-                                                        <th>Product Created</th>
-                                                        <!-- Add more table headers as needed -->
-                                                    </tr>
-                                                </thead>
-                                            </div>
+                                            
+                                            <thead style="background-color: #f5f5f5; padding: 10px; position: sticky; top: 0; z-index: 1;">
+                                                <tr>
+                                                    <th>Product Type</th>
+                                                    <th>Product Name</th>
+                                                    <th>Product Brand</th>
+                                                    <th>Stocks</th>
+                                                    <th>Price - (pcs)</th>
+                                                    <th>Price - (pck)</th>
+                                                    <th>Product Pack</th>
+                                                    <th>Product Created</th>
+                                                    <!-- Add more table headers as needed -->
+                                                </tr>
+                                            </thead>
                                             <tbody id="productInputsEdit">
                                                 <!-- Initial input fields for product details -->
                                                 <input type="text" name="req" class="form-control" value="edit"
