c8898b99c73fce55d1d734574bba81fe4f8c4d7e
abirmehedi77 <86663285+abirmehedi77@users.noreply.github.com>
2024-01-11 17:27:15 +0800

    Update ReportController.php

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -162,7 +162,7 @@ class ReportController extends Controller
                     
                 $rejected = DB::table('rejecteds')
                     ->join('inventories', 'rejecteds.inventories_id', '=', 'inventories.id')
-                    ->select('id', 'product_type', 'product_name', 'stocks', 'product_pcs_price', 'inventories.product_pcs_per_pack as product_pack_price')
+                    ->select('rejecteds.id', 'rejecteds.inventories_id','rejecteds.product_type', 'rejecteds.product_name', 'rejecteds.stocks', 'rejecteds.product_pcs_price', 'inventories.product_pcs_per_pack as product_pack_price')
                     ->orderBy('created_at', 'asc')
                     ->get();
 
